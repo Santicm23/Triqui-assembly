c0125ac6d78e3b49ac3b19b544abb6b30a3419db
Santicm23 <santicastrom@outlook.com>
2022-03-18 22:04:29 -0500

    Primer diseño de mpresion de matriz

--- main.asm
+++ main.asm
@@ -1,7 +1,39 @@
-.data
+# / - - - / Triqui hecho por Michael Gonzales, Jose Manuel Rodríguez y Santiago Castro / - - - / #
 
+.data
+	borde: .asciiz "  +---+---+---+\n"
+	fila:  .asciiz  " |   |   |   |\n"
+	cols:  .asciiz "    0   1   2  \n"
 .text
-	main:
-		
+	main: #Funcion principal
+		addi $t0, $zero, 0
+		while:
+			bge $t0, 1, exit
+			addi $t0, $t0, 1
+			jal imprimirMatriz #Llamada a la funcion imprimirMatriz
+		j while
+		exit:
 	li $v0, 10
-	syscall
+	syscall #Fin funcion principal
+	
+	imprimirMatriz: #funcion imprimirMatriz
+		addi $t1, $zero, 0
+		while1:
+			bge $t1, 3, exit1
+			li $v0, 4
+			la $a0, borde
+			syscall
+			li $v0, 1
+			addi $a0, $t1, 0
+			syscall
+			li $v0, 4
+			la $a0, fila
+			syscall
+			addi $t1, $t1, 1
+		j while1
+		exit1:
+		la $a0, borde
+		syscall
+		la $a0, cols
+		syscall
+	jr $ra #fin funcion imprimirMatriz
